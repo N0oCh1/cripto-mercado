0f2c8a83d357854c01d52250a7be42edfc971450
N0oCh1 <rfeng@ayudinga.org>
2024-12-10 17:00:03 -0500

    ✨

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 'use client'
+import CardCrip from "@/components/CardCrip";
+import CardSelected from "@/components/CardSelected";
 import  ObtenerNombreCrip  from "@/scripts/GetData";
-import Image from "next/image";
 import { useEffect, useState } from "react";
-import { z } from "zod";
+import { symbol, z } from "zod";
 
 export default function Home() {
   const [outset, setOutset] = useState<number>(0);
   const [limit, setLimit] = useState<number>(50);
+  
 
   const dataSchema = z.array(z.object({
     symbol: z.string(),
@@ -14,7 +16,9 @@ export default function Home() {
   }))
   type DataType = z.infer<typeof dataSchema>
   const [apiData, setApiData] = useState<DataType>()
-  
+
+  // Variable para capturar la moneda seleccionada
+  const [cardSelected, setCardSelected] = useState<DataType>([])
   useEffect(()=>{
     async function fetch() {
       setApiData(await ObtenerNombreCrip())
@@ -23,19 +27,45 @@ export default function Home() {
     const intervalID = setInterval(fetch, 5000)
     return () => clearInterval(intervalID)
   },[])
-
   return (
-    <main className="flex justify-center p-20">
-      <div className="bg-white rounded-xl p-10 text-black w-[50%] min-w-96 flex flex-row flex-wrap gap-5">
+    <main className="flex justify-center p-20 flex-col items-center ">
+      <div className="bg-white rounded-xl p-10 text-black w-[70vw] min-w-96 flex flex-row flex-wrap gap-5 h-[50vh] overflow-y-scroll">
         {apiData && (apiData.slice(outset, limit).map((currency, index)=>{
           return(
-            <div className="flex flex-col bg-black text-white p-3 rounded-xl" key={index}>
-              <p>{currency.symbol}</p>
-              <p>{currency.price}</p>
-            </div>
+            <CardCrip key={index} symbol={currency.symbol} price={currency.price}
+            onClick={()=>{
+              setCardSelected([...cardSelected, {symbol: currency.symbol, price:currency.price}])
+            }}
+            />
           )
         }))}
       </div>
+      <div className="flex flex-row justify-center gap-6">
+          <button className="px-5 bg-white text-black" onClick={()=>{
+            if(outset !== 0){
+              setOutset(outset-50);
+              setLimit(limit-50)
+            }
+          }}>-50</button>
+          <button className="px-5 bg-white text-black" onClick={()=>{
+            if(apiData!.slice(outset, limit).length === 50){
+              setOutset(outset+50);
+              setLimit(limit+50)
+            }
+            }}>+50</button>
+        </div>
+        <div className="flex flex-col justify-center gap-4">
+            {cardSelected && (cardSelected.map((d, index)=> {
+              const dataSelected = apiData?.find(v=>v.symbol === d.symbol)
+              return(
+                <CardSelected key={index}>
+                  <p>{dataSelected?.symbol}</p>
+                  <p>{dataSelected?.price}</p>
+                </CardSelected>
+              )
+            }
+            ))}
+        </div>
     </main>
   );
 }
