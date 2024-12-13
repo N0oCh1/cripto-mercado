b01a737a1c0ebf4219095952f8e86f229d21d1a3
N0oCh1 <rubenfeng12@gmail.com>
2024-12-13 17:17:15 -0500

    🐛 wip: Update design

--- components/CardSelected.tsx
+++ components/CardSelected.tsx
@@ -1,13 +1,25 @@
-import { FC, ReactNode } from "react"
-
-
+import { FC, ReactNode, useRef, useState } from "react"
 interface CardSelectedProps{
+    price?: String
     children?: ReactNode
+    ClassName? : string
 }
-const CardSelected: FC<CardSelectedProps> =({children})=>{
+const CardSelected: FC<CardSelectedProps> =({children, price, ClassName})=>{
+    const inputRef = useRef<HTMLInputElement|null>(null)
+    const [myValue, setMyValue] = useState<string>("0")
+    const [CantidadComprado, setCantidadComprado] = useState<Number>()
+    const handleClick = () => {
+        setMyValue(inputRef.current!.value)
+    }
+    
     return(
-        <div className="flex flex-col px-4 border-white border-2 rounded-xl">
+        <div className={` ${ClassName}`}>
             {children}
+            <input type="number" className="text-black px-3 py-1" ref={inputRef}/>
+            <button onClick={()=>handleClick()}>Comprar</button>
+            <p>
+                Diferencia<br/>{}
+            </p>
         </div>
     )
 }
