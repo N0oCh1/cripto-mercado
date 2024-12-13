31bac9144bd2459bbcd8d1a675991a8cca09554e
N0oCh1 <rubenfeng12@gmail.com>
2024-12-13 17:16:45 -0500

    🐛 fix: Condicional API

--- scripts/GetData.ts
+++ scripts/GetData.ts
@@ -2,6 +2,11 @@ import { z } from "zod"
 
 async function ObtenerNombreCrip() {
     const urlAPI = "https://data-api.binance.vision/api/v3/ticker/price"
-    return await fetch(urlAPI).then(res=>res.json()).then(data=>data)
+    return await fetch(urlAPI).then(res=>res.json()).then(data=>{
+        if(Number(data.price) === 0.0){
+            return
+        }
+        return data
+    })
 }
 export default ObtenerNombreCrip
